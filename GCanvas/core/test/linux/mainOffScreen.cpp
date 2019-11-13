78970208442f9e1eb1a66759ab0f7e9e9dc43261
mingzong.lzw <zhiweiluestc@gmail.com>
2019-11-13 14:39:45 +0800

    m:format the code

--- GCanvas/core/test/linux/mainOffScreen.cpp
+++ GCanvas/core/test/linux/mainOffScreen.cpp
@@ -9,31 +9,34 @@
 #include <iostream>
 #include <GCanvas.hpp>
 #include "lodepng.h"
+#define CONTEXT_ES20
 
-void ConvertPixelsToPng(std::string filename, uint8_t* buffer, int width, int height)
+void ConvertPixelsToPng(std::string filename, uint8_t *buffer, int width, int height)
 {
-    unsigned error = lodepng::encode(filename.c_str(),  buffer,  width,  height);
-    if(error) {
-        std::cout << "encoder error " << error << ": "<< lodepng_error_text(error)  << filename << std::endl;
+    unsigned error = lodepng::encode(filename.c_str(), buffer, width, height);
+    if (error)
+    {
+        std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << filename << std::endl;
     }
 }
 
-void decodeFromFile(const char* filename,  std::vector<unsigned char>& image) 
+void decodeFromFile(const char *filename, std::vector<unsigned char> &image)
 {
-  unsigned width, height;
-  //decode image
-  unsigned error = lodepng::decode(image, width, height, filename);
-  //if there's an error, display it
-  if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+    unsigned width, height;
+    //decode image
+    unsigned error = lodepng::decode(image, width, height, filename);
+    //if there's an error, display it
+    if (error)
+        std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
-    #define CONTEXT_ES20
 
-    #ifdef CONTEXT_ES20
-        EGLint ai32ContextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE };
-    #endif
+
+#ifdef CONTEXT_ES20
+    EGLint ai32ContextAttribs[] = {EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE};
+#endif
 
     // Step 1 - Get the default display.
     EGLDisplay eglDisplay = eglGetDisplay((EGLNativeDisplayType)0);
@@ -41,7 +44,7 @@ int main(int argc, char *argv[])
     // Step 2 - Initialize EGL.
     eglInitialize(eglDisplay, 0, 0);
 
-    #ifdef CONTEXT_ES20
+#ifdef CONTEXT_ES20
     // Step 3 - Make OpenGL ES the current API.
     eglBindAPI(EGL_OPENGL_ES_API);
 
@@ -52,20 +55,21 @@ int main(int argc, char *argv[])
     pi32ConfigAttribs[2] = EGL_RENDERABLE_TYPE;
     pi32ConfigAttribs[3] = EGL_OPENGL_ES2_BIT;
     pi32ConfigAttribs[4] = EGL_NONE;
-    #else
+#else
     EGLint pi32ConfigAttribs[3];
     pi32ConfigAttribs[0] = EGL_SURFACE_TYPE;
     pi32ConfigAttribs[1] = EGL_WINDOW_BIT;
     pi32ConfigAttribs[2] = EGL_NONE;
-    #endif
+#endif
 
     // Step 5 - Find a config that matches all requirements.
     int iConfigs;
     EGLConfig eglConfig;
     eglChooseConfig(eglDisplay, pi32ConfigAttribs, &eglConfig, 1,
-                                                    &iConfigs);
+                    &iConfigs);
 
-    if (iConfigs != 1) {
+    if (iConfigs != 1)
+    {
         printf("Error: eglChooseConfig(): config not found \n");
         exit(-1);
     }
@@ -73,14 +77,14 @@ int main(int argc, char *argv[])
     // Step 6 - Create a surface to draw to.
     EGLSurface eglSurface;
     // eglSurface = eglCreateWindowSurface(eglDisplay, eglConfig,  (EGLNativeWindowType)NULL, NULL);
-    eglSurface = eglCreatePbufferSurface(eglDisplay, eglConfig,   NULL);
+    eglSurface = eglCreatePbufferSurface(eglDisplay, eglConfig, NULL);
     // Step 7 - Create a context.
     EGLContext eglContext;
-    #ifdef CONTEXT_ES20
-        eglContext = eglCreateContext(eglDisplay, eglConfig, NULL,ai32ContextAttribs);
-    #else
-        eglContext = eglCreateContext(eglDisplay, eglConfig, NULL, NULL);
-    #endif
+#ifdef CONTEXT_ES20
+    eglContext = eglCreateContext(eglDisplay, eglConfig, NULL, ai32ContextAttribs);
+#else
+    eglContext = eglCreateContext(eglDisplay, eglConfig, NULL, NULL);
+#endif
 
     // Step 8 - Bind the context to the current thread
     eglMakeCurrent(eglDisplay, eglSurface, eglSurface, eglContext);
@@ -95,74 +99,81 @@ int main(int argc, char *argv[])
     glGenFramebuffers(1, &fboId);
     glBindFramebuffer(GL_FRAMEBUFFER, fboId);
 
-     GLuint renderBuffer;
-     glGenRenderbuffers(1, &renderBuffer);
-     glBindRenderbuffer(GL_RENDERBUFFER, renderBuffer);
-     glRenderbufferStorage(GL_RENDERBUFFER,GL_RGB565, renderBufferWidth,renderBufferHeight);
-     glFramebufferRenderbuffer(GL_FRAMEBUFFER,GL_COLOR_ATTACHMENT0,GL_RENDERBUFFER,renderBuffer);
-      GLuint depthRenderbuffer;
-      glGenRenderbuffers(1, &depthRenderbuffer);
-      glBindRenderbuffer(GL_RENDERBUFFER, depthRenderbuffer);
-      glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16,     renderBufferWidth, renderBufferHeight);
-      glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderbuffer);
+    GLuint renderBuffer;
+    glGenRenderbuffers(1, &renderBuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, renderBuffer);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_RGB565, renderBufferWidth, renderBufferHeight);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, renderBuffer);
+    GLuint depthRenderbuffer;
+    glGenRenderbuffers(1, &depthRenderbuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, depthRenderbuffer);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, renderBufferWidth, renderBufferHeight);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderbuffer);
 
-      // check FBO status
-      GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-      if(status != GL_FRAMEBUFFER_COMPLETE) {
-          printf("Problem with OpenGL framebuffer after specifying color render buffer: n%xn", status);
-      } else {
-          printf("FBO creation succeddedn \n ");
-     }
+    // check FBO status
+    GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+    if (status != GL_FRAMEBUFFER_COMPLETE)
+    {
+        printf("Problem with OpenGL framebuffer after specifying color render buffer: n%xn", status);
+    }
+    else
+    {
+        printf("FBO creation succeddedn \n ");
+    }
 
-     GLint format = 0, type = 0;
-     glGetIntegerv(GL_IMPLEMENTATION_COLOR_READ_FORMAT, &format);
-     glGetIntegerv(GL_IMPLEMENTATION_COLOR_READ_TYPE, &type);
+    GLint format = 0, type = 0;
+    glGetIntegerv(GL_IMPLEMENTATION_COLOR_READ_FORMAT, &format);
+    glGetIntegerv(GL_IMPLEMENTATION_COLOR_READ_TYPE, &type);
 
- printf("run the gcanvas offscreen \n");
- gcanvas:: GCanvas c("id",{false,true},nullptr);
- c.CreateContext();
- c.OnSurfaceChanged(0,0,renderBufferWidth ,renderBufferHeight);   
- c.mCanvasContext->SetFillStyle("#ff0000");
- c.mCanvasContext->FillRect(0,0,renderBufferWidth,renderBufferHeight);
- c.drawFrame();
+    printf("run the gcanvas offscreen \n");
+    gcanvas::GCanvas c("id", {false, true}, nullptr);
+    c.CreateContext();
+    c.OnSurfaceChanged(0, 0, renderBufferWidth, renderBufferHeight);
+    c.mCanvasContext->SetFillStyle("#ff0000");
+    c.mCanvasContext->FillRect(0, 0, renderBufferWidth, renderBufferHeight);
+    c.drawFrame();
 
-  // commit the clear to the offscreen surface
-//   eglSwapBuffers(eglDisplay, eglSurface);
+    // commit the clear to the offscreen surface
+    //   eglSwapBuffers(eglDisplay, eglSurface);
 
-  // You should put your own calculation code here based on format/type
-  // you discovered above
-  int size = 4 * renderBufferHeight * renderBufferWidth;
-  unsigned char *data = new unsigned char[size];
+    // You should put your own calculation code here based on format/type
+    // you discovered above
+    int size = 4 * renderBufferHeight * renderBufferWidth;
+    unsigned char *data = new unsigned char[size];
 
-  printf("the size %d \n", size);
+    printf("the size %d \n", size);
 
-  // in my case, I got back a buffer that was RGB565
-  glReadPixels(0,0,renderBufferWidth,renderBufferHeight,GL_RGBA,GL_UNSIGNED_BYTE, data);
-  ConvertPixelsToPng("a.png",data,renderBufferWidth,renderBufferHeight);
- 
-  delete data;
-  std::vector<unsigned char> standrandImage;
-  std::vector<unsigned char> gcanvasImage;
- decodeFromFile("../../w3c/build/mycanvasPage.png",standrandImage);
- decodeFromFile("a.png",gcanvasImage);
- 
-std::cout << "standrand image "<<std::endl;
-std::cout << "standrand size "<<  standrandImage.size()<< std::endl;
+    // in my case, I got back a buffer that was RGB565
+    glReadPixels(0, 0, renderBufferWidth, renderBufferHeight, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    ConvertPixelsToPng("a.png", data, renderBufferWidth, renderBufferHeight);
 
-std::cout << "mycanvas page image"<<std::endl;
-std::cout << "mycanvas page size "<<  gcanvasImage.size()<< std::endl;
-int N= std::min(standrandImage.size(),gcanvasImage.size());
+    delete data;
+    std::vector<unsigned char> standrandImage;
+    std::vector<unsigned char> gcanvasImage;
+    decodeFromFile("../../w3c/build/mycanvasPage.png", standrandImage);
+    decodeFromFile("a.png", gcanvasImage);
 
-int errorCount=0;
-int rightCount=0;
-for(int i=0;i<N;i++){
-        if(standrandImage[i]!=gcanvasImage[i]){
-                errorCount++;
-        }else{
+    std::cout << "standrand image " << std::endl;
+    std::cout << "standrand size " << standrandImage.size() << std::endl;
+
+    std::cout << "mycanvas page image" << std::endl;
+    std::cout << "mycanvas page size " << gcanvasImage.size() << std::endl;
+    int N = std::min(standrandImage.size(), gcanvasImage.size());
+
+    int errorCount = 0;
+    int rightCount = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (standrandImage[i] != gcanvasImage[i])
+        {
+            errorCount++;
+        }
+        else
+        {
             rightCount++;
         }
-}
+    }
 
-std::cout<< "error Count " <<errorCount <<std::endl;
-std::cout<< "right Count " <<rightCount <<std::endl;
+    std::cout << "error Count " << errorCount << std::endl;
+    std::cout << "right Count " << rightCount << std::endl;
 }
