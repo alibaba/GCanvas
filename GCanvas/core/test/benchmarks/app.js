a20a6bc94388695672ebbd064fd4524f2d11637f
mingzong.lzw <mingzong.lzw@alibaba-inc.com>
2019-11-21 11:33:45 +0800

    m:修改转换器为cli方式，输入casename

--- GCanvas/core/test/benchmarks/app.js
+++ GCanvas/core/test/benchmarks/app.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 var path = require("path");
-var files = ['tc_2d_translate'];
+var caseName = process.argv[2];
 var wstream = fs.createWriteStream(path.resolve("../linux/util/test.cpp"));
 var prefix = `#include <unordered_map>
 #include <functional>
@@ -12,48 +12,47 @@ void prepareCases( std::unordered_map< std::string,std::function<void(std::share
 var postfix = ` }\n`
 
 
-var dict={
-    "ctx.":"ctx->",
-    "Math.PI":"M_PI",
-    "beginPath":"BeginPath",
-    "moveTo":"MoveTo",
-    "lineTo":"LintTo",
-    "stroke(":"Stroke(",
-    "fill(":"Fill(",
-    "fillRect":"FillRect",
-    "arc":"Arc",
-    "arcto":"ArcTo",
-    "bezierCurveTo":"BezierCurveTo",
-    "clip":"Clip",
-    "font":"SetFont(",
-    "save":"Save",
-    "restore":"Restore",
-    "rotate":"Rotate",
-    "scale":"Scale",
-    "translate":"Translate",
-    "strokeRect":"StrokeRect(",
-    "fillText":"FillText",
-    "strokeStyle":"SetStrokeStyle(",
-    "fillStyle":"SetFillStyle(",
-    "lineWidth":"SetLineWidth(",
-    "c.width":"width",
-    "c.height":"height",
-    "quadraticCurveTo":"QuadraticCurveTo",
-    "shadowBlur":"SetShadowBlur(",
-    "shadowOffsetX":"SetShadowOffsetX(",
-    "shadowOffsetY":"SetShadowOffsetY(",
-    "shadowColor":"SetShadowColor(",
+var dict = {
+    "ctx.": "ctx->",
+    "Math.PI": "M_PI",
+    "beginPath": "BeginPath",
+    "moveTo": "MoveTo",
+    "lineTo": "LintTo",
+    "stroke(": "Stroke(",
+    "fill(": "Fill(",
+    "fillRect": "FillRect",
+    "arc": "Arc",
+    "arcto": "ArcTo",
+    "bezierCurveTo": "BezierCurveTo",
+    "clip": "Clip",
+    "font": "SetFont(",
+    "save": "Save",
+    "restore": "Restore",
+    "rotate": "Rotate",
+    "scale": "Scale",
+    "translate": "Translate",
+    "strokeRect": "StrokeRect(",
+    "fillText": "FillText",
+    "strokeStyle": "SetStrokeStyle(",
+    "fillStyle": "SetFillStyle(",
+    "lineWidth": "SetLineWidth(",
+    "c.width": "width",
+    "c.height": "height",
+    "quadraticCurveTo": "QuadraticCurveTo",
+    "shadowBlur": "SetShadowBlur(",
+    "shadowOffsetX": "SetShadowOffsetX(",
+    "shadowOffsetY": "SetShadowOffsetY(",
+    "shadowColor": "SetShadowColor(",
 }
 
 wstream.write(prefix);
-for (let i = 0; i < files.length; i++) {
-    var data = fs.readFileSync(files[i] + ".js");
-    var content = dealFileContent(data.toString());
-    var str = `testCases["${files[i]}"]= [](std::shared_ptr<gcanvas::GCanvas> canvas, GCanvasContext *ctx,int width,int height)
+
+var data = fs.readFileSync(caseName + ".js");
+var content = dealFileContent(data.toString());
+var str = `testCases["${caseName}"]= [](std::shared_ptr<gcanvas::GCanvas> canvas, GCanvasContext *ctx,int width,int height)
     { \n    int  ratio=1; \n  ${content}  };\n`;
-    console.log(str);
-    wstream.write(str);
-}
+console.log(str);
+wstream.write(str);
 
 
 var regex2 = new RegExp('\\w+');
@@ -61,7 +60,7 @@ function dealFileContent(content) {
     return data.toString().replace(
         /ctx.|beginPath|moveTo|lineTo|stroke\(|fill\(|fillStyle|fillRect|c.width|c.height|strokeStyle|arc|arcTo|bezierCurveTo|clip|font|fillText|lineWidth|shadowBlur|shadowOffsetX|shadowOffsetY|shadowColor|save|strokeRect|rotate|restore|scale|translate|Math.PI/g, (matched) => {
             return dict[matched];
-    });
+        });
 }
 wstream.write(postfix);
 
--- GCanvas/core/test/linux/util/test.cpp
+++ GCanvas/core/test/linux/util/test.cpp
@@ -4,12 +4,19 @@
 #include "GCanvas.hpp" 
 void prepareCases( std::unordered_map< std::string,std::function<void(std::shared_ptr<gcanvas::GCanvas> canvas,  GCanvasContext *mCanvasContext,int width,int height)>>  &testCases)
 { 
-  testCases["tc_2d_translate"]= [](std::shared_ptr<gcanvas::GCanvas> canvas, GCanvasContext *ctx,int width,int height)
+  testCases["tc_2d_fill"]= [](std::shared_ptr<gcanvas::GCanvas> canvas, GCanvasContext *ctx,int width,int height)
     { 
     int  ratio=1; 
-  ctx->SetFillStyle( = '#000000';
-ctx->FillRect(10*ratio, 10*ratio, 100*ratio, 50*ratio);
-ctx->Translate(100*ratio, 100*ratio);
-ctx->FillRect(10*ratio, 10*ratio, 100*ratio, 50*ratio);
+  
+ctx->SetFillStyle( = "#000000";
+ctx->FillRect(0, 0, width, height);
+
+    ctx->SetFillStyle( = "#FFF";
+    ctx->SetLineWidth( = 2;
+    ctx->BeginPath();
+    ctx->MoveTo(10*ratio, 24*ratio);
+    ctx->LintTo(100*ratio, 200*ratio);
+    ctx->LintTo(50*ratio, 34*ratio);
+    ctx->Fill();
   };
  }
