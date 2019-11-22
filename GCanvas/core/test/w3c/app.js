c47625ba9aa547834dee09ce613da569c8bda30a
mingzong.lzw <mingzong.lzw@alibaba-inc.com>
2019-11-22 10:51:11 +0800

    m:生成自动目录js文件的逻辑

--- GCanvas/core/test/w3c/app.js
+++ GCanvas/core/test/w3c/app.js
@@ -1,21 +1,19 @@
 
 const puppeteer = require('puppeteer');
 let caseNames = {
-    "path": ["tc_2d_beginPath.js", "tc_2d_closePath.js","tc_2d_lineTo.js","tc_2d_moveTo.js"],
-    "fill": ["tc_2d_clearRect.js", "tc_2d_fillRect.js", "tc_2d_fill.js", "tc_2d_text_fillText.js"],
-    "verb": ["tc_2d_MDN_scale.js", "tc_2d_resetTransform.js", "tc_2d_scale.js",
-        "tc_2d_MDN_transform.js", "tc_2d_rotate.js", "tc_2d_transform.js",
-        "tc_2d_MDN_translate.js", "tc_2d_save.js", "tc_2d_translate.js",],
-    "line": ["tc_2d_bezierCurveTo.js", "tc_2d_lineGap_square.js", "tc_2d_lineTo.js",
-        "tc_2d_line_dash.js", "tc_2d_lineJoin_bevel.js", "tc_2d_MDN_lineTo.js",
-        "tc_2d_lineGap_butt.js", "tc_2d_lineJoin_miter.js", "tc_2d_quadraticCurveTo.js",
-        "tc_2d_lineGap_round.js", "tc_2d_lineJoin_round.js",],
-    "font": ["tc_2d_text_fillText.js"],
-    "stroke": ["tc_2d_strokeRect.js", "tc_2d_stroke_alpha.js",
-        "tc_2d_stroke.js", "tc_2d_strokeStyle.js"],
-    "shaow": [
-        "tc_2d_shadow.js", "tc_2d_shadowOffsetY.js",
-        "tc_2d_shadowOffsetX.js", "tc_2d_shaow_blur.js",]
+    "arc" :["tc_2d_MDN_arc.js","tc_2d_MDN_arcTo_1.js","tc_2d_MDN_arcTo_2.js","tc_2d_arc.js","tc_2d_arcTo.js",],
+"clip" :["td_2d_clip.js",],
+"composite" :["compite_desover.js","composite_sourceover.js",],
+"eclipse" :["tc_2d_ellipse.js",],
+"fill" :["tc_2d_clearRect.js","tc_2d_fill.js","tc_2d_fillRect.js","tc_2d_text_fillText.js",],
+"fillstyle" :["tc_2d_createRadialGradient.js","tc_2d_globalAlpha.js","tc_2d_lineargradient.js","tc_2d_pattern_no_repeat.js","tc_2d_pattern_repeat.js","tc_2d_pattern_repeat_x.js","tc_2d_pattern_repeat_y.js","tc_2d_radialgradient.js",],
+"font" :["tc_2d_font.js","tc_2d_textBaseline.js","tc_2d_text_fillText.js","tc_2d_text_fillText_alpha.js","tc_2d_text_measureText.js","tc_2d_text_strokeText.js","tc_2d_text_textAlign.js",],
+"image" :["tc_2d_createImageData.js","tc_2d_drawImage.js","tc_2d_get_put_ImageData.js",],
+"line" :["tc_2d_MDN_lineTo.js","tc_2d_bezierCurveTo.js","tc_2d_lineGap_butt.js","tc_2d_lineGap_round.js","tc_2d_lineGap_square.js","tc_2d_lineJoin_bevel.js","tc_2d_lineJoin_miter.js","tc_2d_lineJoin_round.js","tc_2d_lineTo.js","tc_2d_line_dash.js","tc_2d_quadraticCurveTo.js",],
+"path" :["tc_2d_beginPath.js","tc_2d_closePath.js","tc_2d_lineTo.js","tc_2d_moveTo.js",],
+"shaow" :["tc_2d_shadow.js","tc_2d_shadowOffsetX.js","tc_2d_shadowOffsetY.js","tc_2d_shaow_blur.js",],
+"stroke" :["tc_2d_stroke.js","tc_2d_strokeRect.js","tc_2d_strokeStyle.js","tc_2d_stroke_alpha.js",],
+"verb" :["tc_2d_MDN_scale.js","tc_2d_MDN_transform.js","tc_2d_MDN_translate.js","tc_2d_resetTransform.js","tc_2d_rotate.js","tc_2d_save.js","tc_2d_scale.js","tc_2d_transform.js","tc_2d_translate.js",],
 };
 const fs = require('fs');
 var path = require("path");
@@ -60,7 +58,7 @@ async function genStandard(caseName) {
     } else {
         console.log("canvas element not exist");
     }
-    console.log("finish the canvas page" + caseName);
+    console.log("finish the canvas page  " + caseName);
     await browser.close();
 }
 
