2e5272e3a8d4d9cd42727ade87d02f1689d09fc2
mingzong.lzw <mingzong.lzw@alibaba-inc.com>
2020-01-08 21:27:43 +0800

    m:补充image.js案例情况:增加image回调

--- node/src/example/image.js
+++ node/src/example/image.js
@@ -2,12 +2,17 @@ const { createCanvas, Image } = require('bindings')('canvas');
 let canvas = createCanvas(400, 400);
 let ctx = canvas.getContext('2d');
 const img = new Image()
+const img2 = new Image()
 
 img.onload = () => {
     ctx.fillStyle = "#000000";
-    ctx.fillRect(0,0,ctx.canvas.width,ctx.canvas.height);
-    ctx.drawImage(img, 0, 0);
-    canvas.createPNG("image");
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.drawImage(img, 0, 0, 100, 100);
+    img2.onload = () => {
+        ctx.drawImage(img2, 150, 150, 100, 100);
+        canvas.createPNG("image");
+    }
+    img2.src = "https://mat1.gtimg.com/pingjs/ext2020/qqindex2018/dist/img/qq_logo_2x.png"
 }
 img.onerror = err => {
     console.log(err)
