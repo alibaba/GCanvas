53ec636b82be50f1f732e20dc1a1dc63ec252962
zhiwei <zhiweiluestc@gmail.com>
2020-06-01 10:19:28 +0800

    chore: add the comment to open source header in file

--- node/util/NodeBindingUtil.h
+++ node/util/NodeBindingUtil.h
@@ -1,5 +1,13 @@
-#ifndef UTIL_H
-#define UTIL_H
+/**
+ * Created by G-Canvas Open Source Team.
+ * Copyright (c) 2017, Alibaba, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache Licence 2.0.
+ * For the full copyright and license information, please view
+ * the LICENSE file in the root directory of this source tree.
+ */
+#ifndef NODE_BINDING_UTIL_H
+#define NODE_BINDING_UTIL_H
 #include <napi.h>
 #define TIMEOUT_VALUE 5L
 namespace NodeBinding
