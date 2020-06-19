a59facf13c4ae7d0288cbbe98aac175845abc112
mingzong.lzw <mingzong.lzw@alibaba-inc.com>
2020-06-19 13:15:23 +0800

    test:补齐shadow path相关测试案例

--- node/examples/textshadow.js
+++ node/examples/textshadow.js
@@ -5,18 +5,18 @@ var context = canvas.getContext('2d');
 context.fillStyle="#ffffff"
 context.fillRect(0,0,canvas.width,canvas.height)
 context.fillStyle="#000000"
-// context.font = "18pt Helvetica";
-// context.fillText("No shadow", 90, 40);
+context.font = "18pt Helvetica";
+context.fillText("No shadow", 90, 40);
 
 context.shadowOffsetX = 3;
 context.shadowOffsetY = 3;
-// context.fillText("No shadow", 90, 80);
+context.fillText("No shadow", 90, 80);
 
 context.shadowColor = "rgba(0,0,0,0.3)";
 context.fillText("shadow", 90, 120);
 
-// context.shadowBlur = 4;
-// context.fillText("Smooth shadow", 90, 160);
+context.shadowBlur = 4;
+context.fillText("Smooth shadow", 90, 160);
 
 const fs = require('fs')
 const path = require('path');
